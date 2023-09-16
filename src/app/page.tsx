ebeb4dff4a15c256c4e75d938dd267f6aeeea819
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2023-09-16 16:52:09 +0900

    :lipstick: Create panda sample page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
-import { css } from '../../styled-system/css'
+import { css, cva } from '../../styled-system/css'
+import { styled } from '../../styled-system/jsx'
+
+const buttonRecipe = cva({
+  base: { cursor: 'pointer', padding: '4', fontSize: '12px' },
+  variants: {
+    visual: {
+      solid: { bg: 'red.500', color: 'white' },
+      outline: { borderWidth: '1px', borderColor: 'red.500' },
+    },
+    size: {
+      sm: { padding: '4', fontSize: '12px' },
+      lg: { padding: '8', fontSize: '20px' },
+    },
+  },
+  compoundVariants: [
+    {
+      visual: 'outline',
+      size: 'lg',
+      css: {
+        borderWidth: '4px',
+        borderColor: 'black',
+      },
+    },
+  ],
+  defaultVariants: { visual: 'outline', size: 'lg' },
+})
+
+const Button = styled('button', buttonRecipe)
 
 export default function Home() {
-  return <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
+  return (
+    <>
+      <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
+      <button className={buttonRecipe({ visual: 'outline', size: 'sm' })}>Hello 🐼!</button>
+      <div>
+        <Button visual="solid">Panda Component</Button>
+      </div>
+    </>
+  )
 }
