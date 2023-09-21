f3bbda0994a98da333ed049e5106ecc6a5ceacef
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2023-09-22 01:10:51 +0900

    :poop: Split recipes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,6 @@
-import { css, cva } from '../../styled-system/css'
+import { css } from '../../styled-system/css'
 import { styled } from '../../styled-system/jsx'
-
-const buttonRecipe = cva({
-  base: { cursor: 'pointer', padding: '4', fontSize: '12px' },
-  variants: {
-    visual: {
-      solid: { bg: 'red.500', color: 'white' },
-      outline: { borderWidth: '1px', borderColor: 'red.500' },
-    },
-    size: {
-      sm: { padding: '4', fontSize: '12px' },
-      lg: { padding: '8', fontSize: '20px' },
-    },
-  },
-  compoundVariants: [
-    {
-      visual: 'outline',
-      size: 'lg',
-      css: {
-        borderWidth: '4px',
-        borderColor: 'black',
-      },
-    },
-  ],
-  defaultVariants: { visual: 'outline', size: 'lg' },
-})
+import { buttonRecipe } from './recipes'
 
 const Button = styled('button', buttonRecipe)
 
