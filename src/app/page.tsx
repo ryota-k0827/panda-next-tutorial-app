dd6e9c841b161499b315959b7d68a3ce6beb7010
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2023-09-22 02:34:07 +0900

    :lipstick: Create todo base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,56 @@
-import { css } from '../../styled-system/css'
-import { styled } from '../../styled-system/jsx'
-import { buttonRecipe } from './recipes'
+import { css, cva } from '../../styled-system/css'
 
-const Button = styled('button', buttonRecipe)
+const buttonRecipe = cva({
+  base: { fontSize: 'xl', mx: '2' },
+  variants: {
+    visual: {
+      add: {color: 'green.500'},
+      toggle: {color: 'gray.500'}
+    }
+  }
+})
+
+const todoListWrapperRecipe = cva({
+  base: { w: '460px', display: 'flex', justifyContent: 'space-between', border: 'solid', borderColor: 'gray.400', mb: '4px', pl: '8px' },
+})
 
 export default function Home() {
   return (
     <>
-      <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
-      <button className={buttonRecipe({ visual: 'outline', size: 'sm' })}>Hello 🐼!</button>
-      <div>
-        <Button visual="solid">Panda Component</Button>
+      <div className={css({ fontSize: '2xl', fontWeight: 'bold', textAlign: 'center' })}>🐼 + Next.js13 ToDo App</div>
+      <div className={css({ display: 'grid', justifyContent: 'center' })}>
+        <div className={todoListWrapperRecipe()}>
+          <h2>No Status</h2>
+          <div>
+            <button className={buttonRecipe({ visual: 'add' })}>+</button>
+            <button className={buttonRecipe({ visual: 'toggle' })}>&or;</button>
+          </div>
+        </div>
+        {/* <ul></ul> */}
+        <div className={todoListWrapperRecipe()}>
+          <h2>ToDo</h2>
+          <div>
+            <button className={buttonRecipe({ visual: 'add' })}>+</button>
+            <button className={buttonRecipe({ visual: 'toggle' })}>&or;</button>
+          </div>
+          {/* <ul></ul> */}
+        </div>
+        <div className={todoListWrapperRecipe()}>
+          <h2>In Progress</h2>
+          <div>
+            <button className={buttonRecipe({ visual: 'add' })}>+</button>
+            <button className={buttonRecipe({ visual: 'toggle' })}>&or;</button>
+          </div>
+          {/* <ul></ul> */}
+        </div>
+        <div className={todoListWrapperRecipe()}>
+          <h2>Done</h2>
+          <div>
+            <button className={buttonRecipe({ visual: 'add' })}>+</button>
+            <button className={buttonRecipe({ visual: 'toggle' })}>&or;</button>
+          </div>
+          {/* <ul></ul> */}
+        </div>
       </div>
     </>
   )
